008b509b43057b332d8907236aa2cb8b978bc911
Hloij <dima1663@gmail.com>
2022-09-23 13:37:08 +0300

    Добавьте файлы проекта.

---
+++ Practika4/Practika4/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Practika4
+{
+    public partial class MainPage : TabbedPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        public void RadioClick_button()
+        {
+
+        }
+
+       
+    }
+}
---
+++ Practika4/Practika4/Page2.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Practika4
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page2 : ContentPage
+    {
+        public Page2()
+        {
+            InitializeComponent();
+        }
+
+        private void Switch_Toggled(object sender, ToggledEventArgs e)
+        {
+            if (Swit.IsToggled == false)
+            {
+                Body.IsEnabled = false;
+            }
+            else
+            {
+                Body.IsEnabled = true;
+            }
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            if (Zima.IsChecked == true) DisplayAlert("Месяца", "Декабрь, Январь, Февраль", "Ок");
+            if (Vesna.IsChecked == true) DisplayAlert("Месяца", "Март, Апрель, Май", "Ок");
+            if (Leto.IsChecked == true) DisplayAlert("Месяца", "Июнь, Июль, Август", "Ок");
+            if (Osen.IsChecked == true) DisplayAlert("Месяца", "Сентябрь, Октябрь, Ноябрь", "Ок");
+        }
+    }
+}
